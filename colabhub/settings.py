43422daadcf04b2deebd62e54212b3b81bca78cd
Khushboo <khushboothakur9876@gmail.com>
2023-05-10 14:02:01 +0530

    add requirements.txt

--- colabhub/settings.py
+++ colabhub/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+e7bc+sq!emsluaiht%-p7932$b_^%%ke!((62g!j&4!-yfj5y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,14 +75,16 @@ WSGI_APPLICATION = 'colabhub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES={
+    "default":dj_database_url.parse("postgres://khushboo:jZ7mjI4ZZkiXDeHARn8QfkJcbVzHBtb4@dpg-chdkflqk728nnn7tnhrg-a/test_4qsn")
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
--- colabhub/urls.py
+++ colabhub/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')) # goes to this file and that handles the routing
 ]
+urlpatterns+=staticfiles_urlpatterns()
